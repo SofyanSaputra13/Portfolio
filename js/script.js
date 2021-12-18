a84ba34127153b2ea4e939e0fd14d269809dd024
SofyanSaputra13 <sofyan.saputra@students.ac.id>
2021-12-18 23:03:13 +0800

    Latest Commit

--- js/script.js
+++ js/script.js
@@ -5,21 +5,15 @@ let currentText = '';
 let words = '';
 
 (function ngetik() {
-
     if (count == txtElement.length) {
         count = 0;
     }
-
     currentText = txtElement[count];
-
     words = currentText.slice(0, ++txtIndex);
     document.querySelector('.efek-ngetik').textContent = words;
-
     if (words.length == currentText.length) {
         count++;
         txtIndex = 0;
     }
-
     setTimeout(ngetik, 500);
-
 })();
