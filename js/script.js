0bb9119f16071933470ce4ad34a1f4dad56adcfc
Sofyan <sofyanadi.saputra01@gmail.com>
2023-07-26 18:38:42 +0700

    Update Portfolio

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-const txtElement = ['Student', 'Web Developer', 'Gamer'];
+const txtElement = ["Freelancer", "Web Developer", "Gamer"];
 let count = 0;
 let txtIndex = 0;
-let currentText = '';
-let words = '';
+let currentText = "";
+let words = "";
 
 (function ngetik() {
-    if (count == txtElement.length) {
-        count = 0;
-    }
-    currentText = txtElement[count];
-    words = currentText.slice(0, ++txtIndex);
-    document.querySelector('.efek-ngetik').textContent = words;
-    if (words.length == currentText.length) {
-        count++;
-        txtIndex = 0;
-    }
-    setTimeout(ngetik, 500);
+  if (count == txtElement.length) {
+    count = 0;
+  }
+  currentText = txtElement[count];
+  words = currentText.slice(0, ++txtIndex);
+  document.querySelector(".efek-ngetik").textContent = words;
+  if (words.length == currentText.length) {
+    count++;
+    txtIndex = 0;
+  }
+  setTimeout(ngetik, 500);
 })();
